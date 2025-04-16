fbf328573eae80d9c2ac0c9ff7fb86b1bb89969c
Suman Samanta <suman@centroxy.com>
2025-04-16 13:53:32 +0530

    fixing the issues

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const app = require('express')();
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from CI/CD!' });
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
 
-module.exports = app;
+// Gracefully shut down the server after the tests are done
+afterAll(() => {
+  server.close();
+});
 
