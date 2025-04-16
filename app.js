7f94ad2d9052748e89310aa4bd52e9efa3c1645d
Suman Samanta <suman@centroxy.com>
2025-04-16 12:21:55 +0530

    Changed the message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from CI/CD!' });
+  res.json({ message: 'Hello from CI/CD! Adding some changes here' });
 });
 
 app.listen(port, () => {
